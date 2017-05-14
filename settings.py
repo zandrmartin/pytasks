d7c571e73cfdd868b2061483be8e94e319e2819c
Zandr Martin <git@zandr.me>
2017-05-14 13:14:32 -0400

    fix data_file path in settings

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 
 xdg_data_home = os.getenv('XDG_DATA_HOME')
 if xdg_data_home is not None:
-    data_file = os.path.join(xdg_data_home, 'pytasks.json')
+    data_file = os.path.join(xdg_data_home, 'tasks.json')
 else:
     data_file = os.path.join(os.getenv('HOME'), '.tasks.json')
 
